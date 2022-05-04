124ff9d59f0ec53e2df62b090adfaccb20617310
Jacob August Davison <daviso53@msu.edu>
2022-05-04 15:17:13 -0400

    Initial commit

---
+++ utils/get_log_data.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def get_log_data(data_path):
+    """Read .log.imsrg data from TCIMSRG.
+    """
+    with open(data_path, 'r') as f:
+        
+        lines = f.readlines()
+
+        lines_trunc = lines[7:]
+        lines_trunc = lines_trunc[:-1]
+
+        print(len(lines), len(lines_trunc))
+
+        data_matrix = np.array([np.array(line.split(','))[0:-1] for line in lines_trunc], dtype=np.float64).T
+
+
+    return data_matrix
